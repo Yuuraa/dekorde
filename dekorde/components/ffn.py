36667ea5cf892acc0d23399fbebfa8f434ac0fdd
yura <yoorachoi8937@gmail.com>
2021-10-17 15:42:01 +0900

    Change importing

--- dekorde/components/ffn.py
+++ dekorde/components/ffn.py
@@ -1,17 +1,18 @@
 
 import torch
+import torch.nn as nn
 
 
-class FeedForward(torch.nn.Module):
+class FeedForward(nn.Module):
     """
     position-wise feedforward network.
     """
     def __init__(self, hidden_size: int):
         super().__init__()
-        self.layers = torch.nn.Sequential(
-            torch.nn.Linear(hidden_size, 2048),
-            torch.nn.ReLU(),
-            torch.nn.Linear(2048, hidden_size)
+        self.layers = nn.Sequential(
+            nn.Linear(hidden_size, 2048),
+            nn.ReLU(),
+            nn.Linear(2048, hidden_size)
         )
 
     def forward(self, H_all: torch.Tensor):
